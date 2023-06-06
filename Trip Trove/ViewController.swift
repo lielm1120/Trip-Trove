40ee5082e413841ad30cd14de39ed8d20c5eb9b9
liel machlufliel machluf <lielm1120@gmail.com>
2023-06-06 22:10:36 +0300

    Initial Commit

---
+++ Trip Trove/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trip Trove
+//
+//  Created by Liel Machluf on 6/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
